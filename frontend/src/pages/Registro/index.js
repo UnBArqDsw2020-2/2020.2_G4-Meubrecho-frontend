585efa07a5afd5c862acd11e5682ac411fa3b225
lucaslop <lucaslopes05597@gmail.com>
2021-03-27 17:07:00 -0300

    #3 Tela inicial pronta

--- frontend/src/pages/Registro/index.js
+++ frontend/src/pages/Registro/index.js
@@ -1,19 +1,60 @@
 import React from 'react';
 
-import { Container,Inicio,Final} from './styles';
-
+import { Container,Inicio,Final,Header,Conteudo,Termos} from './styles';
+import { Form, Input } from "@rocketseat/unform";
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/cart.png';
+import Foto from '../../assets/Foto.png';
 function Registro() {
+
+    function handleSubmit(data){
+        console.log(data);
+    }
+
+
   return (
   
    <Container>
        <Inicio>
+            <Header>
+                <img src={Logo} alt="logo"/>
+                <text>MB</text>
+                <h1>Meu Brechó</h1>
+            </Header>
 
+
+            <h2> Compre mais e pague menos</h2>
+
+            <Conteudo>   
+                
+                <img src={Foto} alt="foto" />
+
+            </Conteudo>  
        </Inicio>
 
-       <Final>
 
-           
+
+       <Final>
+           <Link to="/login">Acessar</Link>
+           <h2>Registra-se</h2>
+
+            <Form onSubmit={handleSubmit}>
+                <Input name="nome" type="text" placeholder="Seu nome" />
+                <Input name="Whatsapp" type="text" maxlength="11" pattern="([0-9]{11})" placeholder="Seu Whatsapp" />
+                <Input name="email" type="email" placeholder="Seu e-mail" />
+                <Input name="CPF" type="text" maxlength="11" pattern="([0-9]{11})" placeholder="Seu CPF" />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="Sua senha"
+                    /> 
+                <button type="submit">Cadastrar</button> 
+            </Form>
+
+            <Termos>ao se cadastrar, você aceita os  <Link to="/termos">Termos de uso</Link></Termos>
+
        </Final>
+       
    </Container>
   );
 }
--- frontend/src/pages/Registro/styles.js
+++ frontend/src/pages/Registro/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+import { darken } from 'polished';
 
 export const Container = styled.div`
   display: flex;
@@ -11,19 +11,144 @@ export const Container = styled.div`
 export const Inicio = styled.div`
    
   width: 70%;
-  background: black;
-    
-    
+  background: #503E8E;
+  display:flex;
+  flex-direction:column;
+
+
+  h2{
+    color: #eee;
+    font-size:22px;
+    margin-left:320px;
+    margin-top: 15px;
+  }
+   
+ 
 `;
+
+
+export const Header = styled.div`
+
+  display:flex;
+  align-items: center;
+  margin:15px;
+
+  img{
+    width:50px;
+    height:50px;
+  }
+
+  text{
+    color: #fff;
+    font-size: 30px;
+    margin-left: 5px;
+  }
+
+  h1 {
+    margin: 0 auto;
+    color: #FFFF;
+    font-size: 60px;
+  }
+
+`
+
+export const Conteudo = styled.div`
+
+img{
+    width: 550px;
+    height:520px;
+    margin-left: 200px;
+  }
+
+
+`
+
+
+
+
 
 
 export const Final = styled.div`
 
-    width:30%;
-    background: #7159c1;
- 
+
+  width:30%;
+  background: #303047;
+  display:flex;
+  flex-direction: column;
   
-  
+  a{
+    color: #eee;
+    font-size: 20px;
+    align-self: flex-end;
+    margin: 22px 20px;
+  }
+
+  h2{
+    color: #ccc;
+    font-size:30px;
+    font-weight: normal;
+    margin-left: 20px;
+  }
     
-    
+  form{ 
+
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+
+    input {
+      margin: 8px 15px;
+      background: rgba(0, 0, 0, 0.3);
+      border: 0;
+      border-radius: 4px;
+      height: 55px;
+      padding: 15px;
+      color: #fff;
+     
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+
+    button {
+      margin: 15px 15px;
+      height: 60px;
+      background: #503E8E;
+      font-weight: normal;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.3s;
+      &:hover {
+        background: ${darken(0.03, '#503E8E')};
+      }
+    }
+
+  }
+
+
 `;
+
+
+export const Termos = styled.p`
+  display: flex;
+  align-self: center;
+  justify-self:center;
+  align-content:center;
+  font-size: 10px;
+  color: #ccc;
+
+
+  a{
+    font-size:10px;
+    margin: 0 10px;
+    padding:0;
+  
+  }
+  
+  
+
+`
+
