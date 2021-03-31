8167905cd9bc073edac0d4a7e6ad062f0e05996f
Victor Peixoto <victorlevipeixoto@gmail.com>
2021-03-31 17:53:12 -0300

    Merge with master

---
+++ src/pages/Login/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { Container, Header, Content, Texto } from './styles';
+import LoginFoto from '../../assets/loginfoto.png';
+import Logo from '../../assets/cart.png';
+import { Form, Input } from '@rocketseat/unform';
+import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
+import { signInRequest } from '../../store/modules/auth/actions';
+import { useDispatch } from 'react-redux';
+
+const schema = Yup.object().shape({
+  email: Yup.string().email('Insira um email válido').required('O e-mail é obrigatorio'),
+  senha: Yup.string().required('A senha é obrigatoria')
+});
+
+function Login() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, senha }) {
+    dispatch(signInRequest(email, senha));
+  }
+
+  return (
+    <Container>
+      <Header>
+        <img src={Logo} alt='foto de login' />
+        <h1>Meu Brechó</h1>
+      </Header>
+      <hr />
+
+      <Content>
+        <img src={LoginFoto} alt='FotoLogin' />
+
+        <Form schema={schema} onSubmit={handleSubmit}>
+          <h2> Faça seu Login</h2>
+          <Input name='email' type='email' placeholder='Seu e-mail' />
+          <Input name='senha' type='password' placeholder='Sua senha' />
+          <button type='submit'>Fazer Login</button>
+          <Texto>
+            Não tem conta? <Link to={'/'}>Crie uma</Link>{' '}
+          </Texto>
+        </Form>
+      </Content>
+    </Container>
+  );
+}
+
+export default Login;
---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,64 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import api from '../../../services/api';
+import history from '../../../services/history';
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, senha } = payload;
+    const response = yield call(api.post, 'sessions', {
+      email,
+      senha
+    });
+
+    const { token, user } = response.data;
+
+    api.defaults.headers.Authorization = `Baerer ${token}`;
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/home');
+  } catch (err) {
+    toast.error('Falha na autenticação, verifique seu email/senha');
+    yield put(signFailure());
+  }
+}
+
+export function* signUp({ payload }) {
+  try {
+    const { nome, whatsapp, email, cpf, senha } = payload;
+
+    yield call(api.post, 'users', {
+      nome,
+      whatsapp,
+      email,
+      cpf,
+      senha
+    });
+
+    history.push('/home');
+  } catch (err) {
+    toast.error('Falha no cadastro verifique seus dados!');
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+  const { token } = payload.auth;
+  if (token) {
+    api.defaults.headers.Authorization = `Baerer ${token}`;
+  }
+}
+
+export function signOut() {
+  history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut)
+]);
