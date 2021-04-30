6069b39071d4e98d6b4d652978c6bc42d9b9cd6a
Victor Peixoto <victorlevipeixoto@gmail.com>
2021-04-30 11:31:13 -0300

    #4 Adição da validação de props

--- frontend/src/components/item/item.js
+++ frontend/src/components/item/item.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './item.css';
+import PropTypes from 'prop-types';
 import Camisa from '../../assets/plain-tshirt.jpg';
 import { Stars as StarsIcon } from '@material-ui/icons';
 import { Button } from '@material-ui/core';
 
-const Item = (name, image, description, price, tag) => {
+const Item = ({ name, image, description, price, tag }) => {
+  console.log('name', name);
   return (
     <div className='item'>
       <div className='image-product'>
@@ -20,6 +22,14 @@ const Item = (name, image, description, price, tag) => {
       </Button>
     </div>
   );
+};
+
+Item.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired
 };
 
 export default Item;
