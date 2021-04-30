3df6e8ce38056f261d82a305f8ca86c1296ea77b
Victor Peixoto <victorlevipeixoto@gmail.com>
2021-04-30 11:31:33 -0300

    #4 Correção de erros

--- frontend/src/pages/Loja/productsList.js
+++ frontend/src/pages/Loja/productsList.js
@@ -4,11 +4,12 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import Item from '../../components/item/item';
 
 function listaProdutos() {
-  const [products, setProducts] = useState(null);
+  const [products, setProducts] = useState([]);
   const getProducts = () => {
     let token;
     token = localStorage.getItem('authToken');
-    if (products === null) {
+    if (products.length === 0) {
+      console.log('teste', products);
       var myHeaders = new Headers();
       myHeaders.append('Authorization', `Bearer ${token}`);
 
@@ -19,35 +20,38 @@ function listaProdutos() {
       const request = new Request('http://localhost:3333/product', myInit);
       return fetch(request)
         .then(response => response.json())
-        .then(products => {
-          console.log(products);
-          if (products.Error === undefined) setProducts(products);
+        .then(jsonResponse => {
+          console.log('api', jsonResponse);
+          if (jsonResponse.Error === undefined) setProducts(jsonResponse);
           else setProducts({ error: 'Sem Protutos na loja' });
+          return products;
         });
     } else return products;
   };
-  const renderFav = () => {
-    getProducts();
+  getProducts();
+  const renderProducts = () => {
     console.log('products', products);
     if (products === null) return <CircularProgress />;
-    else if (products.error !== null || products.error !== undefined)
+    else if (products.error !== null && products.error !== undefined)
       return <h1 style={{ color: 'red' }}>A loja não tem nenhum produto no momento</h1>;
     else {
       return products.map((product, index) => (
         <div key={index}>
           <Item
-            name={product.name}
-            image={product.image}
-            description={product.description}
-            price={product.price}
-            tag={product.tag}
+            name={product.nome}
+            image={product.imagem}
+            description={product.descricao}
+            price={product.preco}
+            tag=''
           />
         </div>
       ));
     }
   };
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'center' }}>{renderFav()}</div>
+    <div style={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'center' }}>
+      {renderProducts()}
+    </div>
   );
 }
 
