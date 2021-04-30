388470f9dfc387f7b2a18c1a56156795e6294987
Victor Peixoto <victorlevipeixoto@gmail.com>
2021-04-30 14:30:30 -0300

    #9 melhoria nos items para receberem ações

--- frontend/src/components/item/item.js
+++ frontend/src/components/item/item.js
@@ -2,33 +2,42 @@ import React from 'react';
 import './item.css';
 import PropTypes from 'prop-types';
 import Camisa from '../../assets/plain-tshirt.jpg';
-import { Stars as StarsIcon } from '@material-ui/icons';
+import { Stars as StarsIcon, DeleteForever as DeleteForeverIcon } from '@material-ui/icons';
 import { Button } from '@material-ui/core';
 
-const Item = ({ name, image, description, price, tag }) => {
-  console.log('name', name);
+const Item = ({ productId, name, image, description, price, tag, buttonConfig, onClick }) => {
+  console.log('id', productId);
   return (
     <div className='item'>
       <div className='image-product'>
         <img src={Camisa} />
       </div>
-      <div className='product-description'>
-        <div className='product-name'>{name}</div>
-        <div className='product-price'>{price}</div>
+      <div className='product-info'>
+        <div className='product-name'>
+          {name
+            .split(' ')
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(' ')}
+        </div>
+        <div className='product-price'>{`${price} R$`}</div>
+        <div className='product-description'>{description}</div>
       </div>
-      <Button className='btn-comprar'>
-        <StarsIcon className='carrinho' />
-        <div>Adicionar aos favoritos</div>
+      <Button className='btn-comprar' onClick={() => onClick(productId)}>
+        {buttonConfig.icon === 'add' ? <StarsIcon color='primary' /> : <DeleteForeverIcon color='error' />}
+        <div>{buttonConfig.text}</div>
       </Button>
     </div>
   );
 };
 
 Item.propTypes = {
+  productId: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
+  buttonConfig: PropTypes.object.isRequired,
+  onClick: PropTypes.func.isRequired,
   tag: PropTypes.string.isRequired
 };
 
